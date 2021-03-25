fca6501a2f547e58d2cffda01700c0dbc9852d90
MastaKoch <alexkoch1996@gmail.com>
2021-03-25 12:09:44 -0400

    got some styles for landing page

--- jamsFinalProject/client/src/components/Nav/index.js
+++ jamsFinalProject/client/src/components/Nav/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './style.css';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <a className="navbar-brand jamLogo" href="/">
+        JAMS
       </a>
     </nav>
   );
---
+++ jamsFinalProject/client/src/pages/LandingPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+
+export default function landingPage() {
+
+    return (
+        <div>
+            <h1>
+                Hello World!
+            </h1>
+
+            {/* 
+            
+            What framework?*
+
+            
+            add a blurb about the compnay "catch phrase"
+            
+            ROW - what makes us so great!
+
+            Currently Signed up Users...
+    
+            about the team
+
+            FAQ
+             */}
+        </div>
+    )
+}
