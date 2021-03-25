5a85a8f28ce0da529cbad8ecd743fdc4fbd9c06b
MastaKoch <alexkoch1996@gmail.com>
2021-03-25 18:48:16 -0400

    more styling to landing page.

--- jamsFinalProject/client/src/components/Nav/index.js
+++ jamsFinalProject/client/src/components/Nav/index.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Button, Menu } from 'semantic-ui-react';
 import './style.css';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
-      <a className="navbar-brand jamLogo" href="/">
-        JAMS
-      </a>
-    </nav>
+      <Menu className="navMenu">
+        <Menu.Item>
+          <Button primary>Sign up</Button>
+        </Menu.Item>
+
+        <Menu.Item>
+          <Button>Log-in</Button>
+        </Menu.Item>
+        <div className="jamDiv">
+           <h1 className="navLogo">JAMS</h1>
+        </div>
+       
+      </Menu>
+
   );
 }
 
--- jamsFinalProject/client/src/pages/LandingPage.js
+++ jamsFinalProject/client/src/pages/LandingPage.js
@@ -1,20 +1,44 @@
 import React from "react";
+import { Container, Header } from 'semantic-ui-react';
+
+
 
 
 
 export default function landingPage() {
 
     return (
-        <div>
-            <h1>
-                Hello World!
-            </h1>
+      <div>
+        <Container text className="container">
+          <Header as="h2" className="head">JAMS</Header>
+          <Header as="h3" className="motto">"Helping children find their voice since 2021."</Header>
+          <p className="aboutJams"><span className="acronym">J</span>ustify yourself.</p>
+          <p className="aboutJams"><span className="acronym">A</span>spire for greater.</p>
+          <p className="aboutJams"><span className="acronym">M</span>ake use of your platform.</p>
+          <p className="aboutJams"><span className="acronym">S</span>ee both sides.</p>
+          <hr />
 
-            {/* 
+          <Header as="h2" className="head">"What is JAMS?"</Header>
+          <p className="aboutJams">
+          
+            JAMS is a creative outlet for students and educators. It gives students the freedom
+            to post their opinions and upvote on their classmates responses on any given topic assigned by their teacher. 
+            
+            Here's how it works:
+          </p>
+          <ul>
+              <li>
+              There is a section for teachers to post articles 
+              "For" or "Against" a certain topic.
+              </li>
+          </ul>
+        </Container>
+
+        {/* 
             
             What framework?*
 
-            
+
             add a blurb about the compnay "catch phrase"
             
             ROW - what makes us so great!
@@ -25,6 +49,6 @@ export default function landingPage() {
 
             FAQ
              */}
-        </div>
-    )
+      </div>
+    );
 }
