de1911c907aa5e40eafbbdb09fdc2d426ff3ae6b
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-04-12 21:58:27 -0400

    Add route file for MVC, commentRoute brings in controller

---
+++ jamsFinalProject/routes/commentRoute.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const commentController = require("../controllers/commentsController");
+
+router.route("/")
+    .get(commentController.findAll)
+    .post(commentController.create);
+
+router.route("/:id")
+    .get(commentController.findById)
+    .put(commentController.update)
+    .delete(commentController.remove);
+
+module.exports = router;
